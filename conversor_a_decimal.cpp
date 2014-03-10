f19e6e87f24775a791161f24b4d348eafdb91b21
Daniel Fernandez Perez <daniel.fdz.perez@gmail.com>
2014-03-10 12:40:17 +0100

    conversor de decimal a binario

--- conversor_a_decimal.cpp
+++ conversor_a_decimal.cpp
@@ -16,50 +16,12 @@ int to_binario(int numero){
 
 }
 
-int puerta_and(int *numero_binario[3]){
-    int *mascara[3];
-    int numero[3];
-    /*mascara[0] =1;
-    mascara[1] =1;
-    mascara[2] =1;*/
-    *mascara[0]=111;
-
-    for(int i=0; i<3; i++){
-	numero[i] = *numero_binario[i] / *mascara[i];
-    }
-
-
-
-    for(int i=0; i<3; i++)
-	printf("%i", numero[i]);
-    printf("\n");
-}
-
 int main(int argc, char *argv[]){
-    /*int numero = atoi(argv[1]),
-      resultado = log2(numero),
-      binario,
-      x = 0;*/
-
-    /*for(int exponente=resultado; exponente >= 0; exponente--){
-      int elevado = exp2(exponente);
-
-      if(numero >= elevado){
-      numero -= elevado;
-      binario += pow (10, exponente);
-      }
-      }	*/
     int numero = atoi(argv[1]);
-	int *binario[3];
-	int binario2;
+	int binario;
+    binario = to_binario(numero);
 
-    binario2 = to_binario(numero);
-    *binario[0]=binario2;
-    /*binario[1]=1;
-    binario[2]=0;*/
-    puerta_and(binario);
-
-    printf("binario= %i\n decimal= %i\n", to_binario(numero), to_decimal(binario2));
+    printf("binario= %i\n decimal= %i\n", to_binario(numero), to_decimal(binario));
     printf("\n");
 
     return EXIT_SUCCESS;
